175f66fd38230423dd971fb8f46340f620579f5c
aladdinalizada <tu7c5zneb@code.edu.az>
2023-06-09 01:23:05 +0400

    complete Offers conpoments

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Offers from "./components/Offers";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Offers />
     </div>
   );
 }

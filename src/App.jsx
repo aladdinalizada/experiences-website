994e7d1b692526a6570c48b1fb4e6f6f8a3da440
aladdinalizada <tu7c5zneb@code.edu.az>
2023-06-11 18:17:22 +0400

    complete rooms components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { Helmet } from "react-helmet";
 import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Offers from "./components/Offers";
 import Plan from "./components/Plan";
+import Rooms from "./components/Rooms";
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Travel Agency</title>
+        <meta name="description" content="Travel Agency" />
+        <meta property="og:title" content="Travel Agency" />
+      </Helmet>
       <Navbar />
       <Hero />
       <Offers />
       <Plan />
+      <Rooms />
     </div>
   );
 }
